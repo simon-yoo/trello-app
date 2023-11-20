a0869d5d2956262100a18c460051959e213210bf
Simon Yoo <yoosghn0@gmail.com>
2023-11-21 12:53:54 +1300

    icon

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,5 +1,20 @@
+import { Medal } from 'lucide-react'
+
 const MarketingPage = () => {
-  return <div>MarketingPage</div>
+  return (
+    <div className='flex items-center justify-center flex-col'>
+      <div className='flex items-center justify-center flex-col'>
+        <div className='mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase'>
+          <Medal className='h-6 w-6 mr-2' />
+          No 1 task managment
+        </div>
+        <h1 className='text-3xl md:text-6xl text-center text-neutral-800 mb-6'>
+          Taskify helps team move
+        </h1>
+        <div></div>
+      </div>
+    </div>
+  )
 }
 
 export default MarketingPage
