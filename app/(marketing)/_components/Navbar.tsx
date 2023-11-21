638ba72c453bcd5f9da185c40bccd494eae1b868
Simon Yoo <yoosghn0@gmail.com>
2023-11-21 13:54:55 +1300

    import logo component

--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -1,8 +1,10 @@
+import Logo from '@/components/Logo'
+
 const Navbar = () => {
   return (
     <div className='fixed-top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center'>
       <div className='md:max-w-screen-2xl mx-auto flex items-center w-full justify-between'>
-        test
+        <Logo />
       </div>
     </div>
   )
