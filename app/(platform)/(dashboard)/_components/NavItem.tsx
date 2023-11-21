368fff97beeee9f0a0fcb344c14190439dc94332
Simon Yoo <yoosghn0@gmail.com>
2023-11-21 19:41:18 +1300

    props and type

--- app/(platform)/(dashboard)/_components/NavItem.tsx
+++ app/(platform)/(dashboard)/_components/NavItem.tsx
@@ -1,4 +1,23 @@
-const NavItem = () => {
+'use client'
+
+export type Organization = {
+  id: string
+  slug: string
+  imageUrl: string
+  name: string
+}
+interface NavItemProps {
+  isActive: boolean
+  isExpanded: boolean
+  organization: Organization
+  onExpand: (id: string) => void
+}
+const NavItem = ({
+  isActive,
+  isExpanded,
+  organization,
+  onExpand,
+}: NavItemProps) => {
   return <div>NavItem</div>
 }
 
