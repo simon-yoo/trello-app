8240ffcf5b08bb61993408d89a1540c8fb78d3c1
Simon Yoo <yoosghn0@gmail.com>
2023-11-21 13:54:46 +1300

    fix error

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -11,12 +11,14 @@ const headingFont = localFont({
 const Logo = () => {
   return (
     <Link href='/'>
-      <div className='hover:opacity-75 transition items-center gap-x-2 hidden md:flex'>
+      <div className='hover:opacity-75 transition  items-center gap-x-2 hidden md:flex'>
         <Image src='/logo.svg' alt='Logo' height={30} width={30} />
+        <p
+          className={cn('text-lg text-neutral-700 pb-1', headingFont.className)}
+        >
+          DOIT
+        </p>
       </div>
-      <p className={cn('text-lg text-neutral-700 pb-1', headingFont.className)}>
-        DOIT
-      </p>
     </Link>
   )
 }
