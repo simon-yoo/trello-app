1f7cd0c36add505d88af27736a6b7d749736dc25
Simon Yoo <yoosghn0@gmail.com>
2023-11-21 19:02:41 +1300

    ing

--- app/(platform)/(dashboard)/_components/Sidebar.tsx
+++ app/(platform)/(dashboard)/_components/Sidebar.tsx
@@ -1,4 +1,16 @@
-const Sidebar = () => {
+'use client'
+
+import Link from 'next/link'
+import { Plus } from 'lucide-react'
+import { useLocalStorage } from 'usehooks-ts'
+import { useOrganization, useOrganizationList } from '@clerk/nextjs'
+
+import { Button } from '@/components/ui/button'
+interface SidebarProps {
+  storageKey?: string
+}
+
+const Sidebar = ({ storageKey }: SidebarProps) => {
   return <div>Sidebar</div>
 }
 
