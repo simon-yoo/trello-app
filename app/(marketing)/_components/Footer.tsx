926ae4278237a7eda3b02260bb6404cbda9d5247
Simon Yoo <yoosghn0@gmail.com>
2023-11-21 14:20:02 +1300

    css

--- app/(marketing)/_components/Footer.tsx
+++ app/(marketing)/_components/Footer.tsx
@@ -1,18 +1,17 @@
 import Logo from '@/components/Logo'
 import { Button } from '@/components/ui/button'
-import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <div className='fixed-bottom-0 w-full px-4 border-t flex'>
+    <div className='fixed bottom-0 w-full p-4 bg-slate-100 border-t flex'>
       <div className='md:max-w-screen-2xl mx-auto flex items-center w-full justify-between'>
         <Logo />
         <div className='space-x-4  md:block md:w-auto flex items-center justify-between w-full'>
-          <Button size='sm' variant='outline' asChild>
-            <Link href='/sign-in'>Login</Link>
+          <Button size='sm' variant='ghost'>
+            Privacy Policy
           </Button>
-          <Button size='sm' asChild>
-            <Link href='/sign-up'>Get DOIT for free</Link>
+          <Button size='sm' variant='ghost'>
+            Terms of Service
           </Button>
         </div>
       </div>
