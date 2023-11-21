335f70b841c5a312ec91bdfa79b958655e776ffc
Simon Yoo <yoosghn0@gmail.com>
2023-11-21 14:32:14 +1300

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,12 @@ export const metadata: Metadata = {
     template: `%s | ${siteConfig.name}`,
   },
   description: siteConfig.description,
+  icons: [
+    {
+      url: '/logo.svg',
+      href: '/logo.svg',
+    },
+  ],
 }
 
 export default function RootLayout({
