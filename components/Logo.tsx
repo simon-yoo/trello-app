3f1a5048ec29c013095e89842ce12b05ec9405e8
Simon Yoo <yoosghn0@gmail.com>
2023-11-21 13:45:21 +1300

    logo component

---
+++ components/Logo.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import localFont from 'next/font/local'
+
+import { cn } from '@/lib/utils'
+
+const headingFont = localFont({
+  src: '../public/fonts/font.woff2',
+})
+
+const Logo = () => {
+  return (
+    <Link href='/'>
+      <div className='hover:opacity-75 transition items-center gap-x-2 hidden md:flex'>
+        <Image src='/logo.svg' alt='Logo' height={30} width={30} />
+      </div>
+      <p className={cn('text-lg text-neutral-700 pb-1', headingFont.className)}>
+        DOIT
+      </p>
+    </Link>
+  )
+}
+
+export default Logo
