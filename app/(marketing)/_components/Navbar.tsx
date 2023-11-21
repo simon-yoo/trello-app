76e361f96553ef56553bed0811fd0555b7342191
Simon Yoo <yoosghn0@gmail.com>
2023-11-21 13:52:04 +1300

    navbar ing

--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -1,5 +1,11 @@
 const Navbar = () => {
-  return <div>navbar</div>
+  return (
+    <div className='fixed-top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center'>
+      <div className='md:max-w-screen-2xl mx-auto flex items-center w-full justify-between'>
+        test
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
